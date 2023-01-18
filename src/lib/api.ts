f765c98b18042516841d47ba5f20b95ff81b10d7
borys35 <boryskac10@gmail.com>
2023-01-18 20:58:55 +0100

    Finish project

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -8,14 +8,14 @@ export async function getProducts(page = 1) {
   const res = await fetch(
     `https://reqres.in/api/products?per_page=${PAGE_SIZE}&page=${page}`
   );
-  if (!res.ok) throw new Error(res.statusText);
+  if (!res.ok) throw new Error(res.status.toString());
   const data = await res.json();
   return data as ArrayResponseType;
 }
 
 export async function getProductById(id: number) {
   const res = await fetch(`https://reqres.in/api/products?id=${id}`);
-  if (!res.ok) throw new Error(res.statusText);
+  if (!res.ok) throw new Error(res.status.toString());
   const data = await res.json();
   return data as SingleResponseType;
 }
